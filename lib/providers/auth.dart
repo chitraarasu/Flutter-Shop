6dce31488a052d06332b00a8894f79017711309a
Chitraarasu.k <75994428+chitraarasu@users.noreply.github.com>
2022-01-23 13:30:03 +0530

    Update auth.dart

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,10 +1,8 @@
 import 'dart:convert';
 import 'dart:async';
-
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
